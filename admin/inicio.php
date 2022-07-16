ce3366a1e7f78a8e3640dff6b305defd66cb9bb7
Maicol Erick Arteaga Guzman <maicolarteaga0711@gmail.com>
2022-07-16 10:34:22 -0400

    SE AGREGO LAS CARPETAS Y REGISTRO Y SE PROCEDERA A UNIR LA RAMA

--- admin/inicio.php
+++ admin/inicio.php
@@ -2,17 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <title>Porfolio-Admin</title>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/newStyle.css">
-    <title>Login</title>
 </head>
 <body>
 <a href="./">Volver</a>
         <form action="" class="form google-font-300">
             <div class="form-item grid-register" >
-                <h1 class="titulo">Registro</h1>
+                <h1 class="titulo">Registrar Proyecto</h1>
                 <input type="text" class="txtnombre" name="nombre" placeholder="Nombre">
                 <input type="text" class="txtapellido" name="apellido" placeholder="Apellido">
                 <input type="email" class="txtemail" name="email" placeholder="Email">
@@ -24,6 +24,5 @@
                 <input type="submit" value="Registrarse" name="enviar" class="btn-enviar"> 
             </div>
         </form>
-
 </body>
 </html>
